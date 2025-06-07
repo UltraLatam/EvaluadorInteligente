2af29fae2818c10eed1f1aab58154e8cb3a6f2aa
UltraLatam <jorge_quispe10@usmp.pe>
2025-06-07 16:40:06 -0500

    Primeros pasos de implementación del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EvaluadorInteligente.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
